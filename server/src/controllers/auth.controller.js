2c943fb5ed2d1a9abea54a6778a6fa5ba2563745
ATPaing <aungthetpaing.2001@gmail.com>
2026-03-10 12:43:50 +0000

    authentication system completed (signup, login, jwt, auth middleware, auth/me endpoint)

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
-import User from '../models/User.model.js';
-import bcrypt from 'bcryptjs';
+import User from "../models/User.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res) => {
     try {
@@ -9,7 +10,7 @@ export const signUp = async (req, res) => {
         const existingUser = await User.findOne({ email });
 
         if (existingUser) {
-            return res.status(400).json({ message: 'User already exists' });
+            return res.status(400).json({ message: "User already exists" });
         }
 
         // Hash the password
@@ -20,13 +21,96 @@ export const signUp = async (req, res) => {
         const user = await User.create({
             name,
             email,
-            passwordHash
+            passwordHash,
         });
 
-        res.status(201).json({ message: 'User created successfully', user});
+        // generate JWT token
+        const token = jwt.sign(
+            {
+                userId: user._id,
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d",
+            },
+        );
 
+        res.status(201).json({
+            message: "User created successfully",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                reliabilityScore: user.reliabilityScore,
+            },
+        });
     } catch (error) {
-        console.error('Error during sign up:', error);
-        res.status(500).json({ message: 'Server error' });
+        console.error("Error during sign up:", error);
+        res.status(500).json({ message: "Server error" });
     }
-}
+};
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        // Check if user exists
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        // Check password
+        const isMatch = await bcrypt.compare(password, user.passwordHash);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        // Generate JWT token
+        const token = jwt.sign(
+            {
+                userId: user._id,
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d",
+            },
+        );
+
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                reliabilityScore: user.reliabilityScore,
+            },
+        });
+    } catch (error) {
+        console.error("Error during login:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+};
+
+export const getMe = async (req, res) => {
+    try {
+        console.log(req.userId)
+        const user = await User.findById(req.userId).select("-passwordHash");
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json({
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            reliabilityScore: user.reliabilityScore,
+        });
+    }
+    catch (error) {
+        console.error("Error fetching user data:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+};
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,9 +1,21 @@
 import express from 'express';
-import { signUp } from '../controllers/auth.controller.js';
+import {
+    signUp,
+    login,
+    getMe
+} from '../controllers/auth.controller.js';
+
+import { protect } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 // Sign up route
 router.post('/signup', signUp);
+
+// Login route
+router.post('/login', login);
+
+// get current user
+router.get('/me', protect, getMe);
 
 export default router;
